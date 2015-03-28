27fb60f9bc028c40543e2e539fcc440baf0c834d
Coma <Coma@powerbot.org>
2015-03-27 20:30:27 -0400

    test

--- javascript/header.js
+++ javascript/header.js
@@ -1,13 +1,13 @@
 var header =
     "<header>" +
     "<h1>RSBot Scripting Tutorials</h1>" +
-    "<span class=\"view\"><a href=\"/index.html\">Home</a></span>" +
+    "<span class=\"view\"><a href=\"./index.html\">Home</a></span>" +
     "<br>" +
-    "<span class=\"view\"><a href=\"/lessons/Contributing.html\">How to Contribute</a></span>" +
+    "<span class=\"view\"><a href=\"./lessons/Contributing.html\">How to Contribute</a></span>" +
     "<br>" +
-    "<span class=\"view\"><a href=\"/lessons/IntelliJ.html\">Setting up RSBot for IntelliJ</a></span>" +
+    "<span class=\"view\"><a href=\"./lessons/IntelliJ.html\">Setting up RSBot for IntelliJ</a></span>" +
     "<br>" +
-    "<span class=\"view\"><a href=\"/lessons/PollingScript.html\">PollingScript</a></span>" +
+    "<span class=\"view\"><a href=\"./lessons/PollingScript.html\">PollingScript</a></span>" +
     "</header>";
 
 $(document).ready(function () {
