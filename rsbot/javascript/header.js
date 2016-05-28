24d37a2835f075af005b0e4812e3dc1957f0fa59
Kyle S <Strikegently@users.noreply.github.com>
2016-05-27 21:13:32 -0400

    fixed https header

--- rsbot/javascript/header.js
+++ rsbot/javascript/header.js
@@ -1,18 +1,8 @@
-var header =
-    "<header>" +
-    "<h1>RSBot in a Nutshell</h1>" +
-    "<span class=\"view\"><a href=\"/rsbot\">Home</a></span>" +
-    "<span class=\"view\"><a href=\"/rsbot/lessons/Contributing.html\">How to Contribute</a></span>" +
-    "<span class=\"view\"><a href=\"/rsbot/lessons/IntelliJ.html\">Setting up RSBot for IntelliJ</a></span>" +
-    "<span class=\"view\"><a href=\"/rsbot/lessons/PollingScript.html\">PollingScript</a></span>" +
-    "<span class=\"view\"><a href=\"/rsbot/lessons/ClientContext.html\">ClientContext</a></span>" +
-    "<span class=\"view\"><a href=\"/rsbot/lessons/QueryIntro.html\">Query Introduction</a></span>" +
-    "<span class=\"view\"><a href=\"/rsbot/lessons/NpcsAndObjects.html\">Npcs and GameObjects</a></span>" +
-    "<span class=\"view\"><a href=\"/rsbot/lessons/Widgets.html\">Widgets and Chat</a></span>" +
-    "<span class=\"view\"><a href=\"/rsbot/lessons/Interacting.html\">Interacting</a></span>" +
-    "<span class=\"view\"><a href=\"/rsbot/lessons/Banking.html\">Banking</a></span>" +
-    "</header>";
-
 $(document).ready(function () {
-    $(".wrapper").prepend(header);
+    var $header = $(".wrapper header");
+    if (!$header.length) {
+        $header = $("<header></header>");
+        $(".wrapper").prepend($header);
+    }
+    $header.load("../header.html");
 });
